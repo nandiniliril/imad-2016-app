962670a63713c099d5444ca094efe584fe3363b9
nandiniliril <nandini.lshah@gmail.com>
2016-09-27 19:52:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,89 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Mother Nature',
+    heading: 'Please Enjoy this Poem',
+    date: '27th September, 2016',
+    content: `
+                <p>I am mother nature 
+                        Which creates and destroys a creature
+                        It took me a long while 
+                        To better the way you live. 
+                        Since you have turned hostile, 
+                        I've got no more to give. </p>
+        
+                    <p>My land is filled with waste 
+                        My children are displaced. 
+                        They live in utter fear, 
+                        Watching friends disappear. </p>
+        
+                    <p>I have become man-made, 
+                        Who serves you like a maid. 
+                        Unnatural and impure, 
+                        A disease with no cure.
+                    </p>
+        
+                    <p>  My children, I love you. 
+                         I hope you love me too. 
+                        Your mother needs you now. 
+                        Help me I'll show you how.  
+                    </p>
+        
+                    <p>Recycle and conserve, 
+                        Heed the rights I deserve. 
+                        Hear my weakening voice, 
+                        Help me regain my poise.  
+                    </p>
+        
+                    <p>My gifts you must protect: 
+                        From air to ocean shelf. 
+                        My love you should respect 
+                        Save me to save yourself.
+                    </p>`
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name ="viewport" content="width=device-width, initial scale =1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        
+        </head>
+        
+        <body>
+            <div class ="container">
+            <div>
+                <a href="/">home</a>
+            </div>
+                ${heading}
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+            </div>
+    </body>
+    
+    </html>
+   `;
+   return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
