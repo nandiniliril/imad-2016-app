fb29f6ecd9fb80ded0d06c4680c9937ca883d81e
nandiniliril <nandini.lshah@gmail.com>
2016-09-29 20:27:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,24 @@
 // counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function (){
-    //Make a request to the counter endpoint
+    //Create a request object
+    var request = new XMLHttpRequest();
     
     //Capture the response and store it in a variable
-    
-    //Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
-}
+    request.onreadystagechange = function (){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+        // Not done
+    };
+    //Make the request
+    request.open('GET', 'http://nandiniliril.imad.hasura-aoo.io/counter', true);
+    request.send(null);
+};
