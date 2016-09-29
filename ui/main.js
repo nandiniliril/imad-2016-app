148a97f8612d5861a084aa974697b8453612cd3d
nandiniliril <nandini.lshah@gmail.com>
2016-09-29 18:39:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+// move the image
+var img = document.getElementById('pencil');
+img.onclick = function(){
+  img.style.marginLeft = '100px';  
+};
